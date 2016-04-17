b98d8593649eedffc2beee4669c86b8f2cb0c95d
shier2nd <shier.2nd@gmail.com>
2016-04-18 04:55:09 +0800

    chapter 25 complete

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -23,6 +23,16 @@ public class FlickrFetchr {
     private static final String TAG = "FlickrFetchr";
 
     private static final String API_KEY = "2e8ffb4622d66fb8d9d8e6866fdeaf5c";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -52,7 +62,17 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public List<GalleryItem> fetchItems(int page) {
+    public List<GalleryItem> fetchRecentPhotos() {
+        String url = buildUrl(FETCH_RECENTS_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query) {
+        String url = buildUrl(SEARCH_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    /*public List<GalleryItem> fetchItems(int page) {
 
         List<GalleryItem> items = new ArrayList<>();
 
@@ -77,6 +97,35 @@ public class FlickrFetchr {
         }
 
         return items;
+    }*/
+//    public List<GalleryItem> fetchItems() {
+    private List<GalleryItem> downloadGalleryItems(String url) {
+
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        } catch (JSONException je) {
+            Log.e(TAG, "Failed to parse JSON: " + je);
+        }catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items" + ioe);
+        }
+
+        return items;
+    }
+
+    private String buildUrl(String method, String query) {
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+
+        if (method.equals(SEARCH_METHOD)) {
+            uriBuilder.appendQueryParameter("text", query);
+        }
+
+        return uriBuilder.build().toString();
     }
 
     private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,17 +1,24 @@
 package com.bignerdranch.android.photogallery;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import com.squareup.picasso.Picasso;
 
@@ -28,8 +35,10 @@ public class PhotoGalleryFragment extends Fragment {
 
     private RecyclerView mPhotoRecyclerView;
     private List<GalleryItem> mItems = new ArrayList<>();
-    private boolean mBackgroundIsLoading;
-    private int mLastFetchedPage;
+//    private boolean mBackgroundIsLoading;
+//    private int mLastFetchedPage;
+    private SearchView mSearchView;
+    private ProgressBar mProgressBar;
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -39,8 +48,10 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        mLastFetchedPage = 1;
-        new FetchItemsTask().execute(mLastFetchedPage);
+        setHasOptionsMenu(true);
+//        mLastFetchedPage = 1;
+//        new FetchItemsTask().execute(mLastFetchedPage);
+        updateItems();
     }
 
     @Nullable
@@ -64,7 +75,7 @@ public class PhotoGalleryFragment extends Fragment {
             }
         });*/
 
-        mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        /*mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
 
             boolean isScrollingUp = false;
 
@@ -91,7 +102,10 @@ public class PhotoGalleryFragment extends Fragment {
                 super.onScrolled(recyclerView, dx, dy);
                 isScrollingUp = (dy > 0);
             }
-        });
+        });*/
+
+        mProgressBar = (ProgressBar) v.findViewById(R.id.fragment_progress_bar);
+        showProgressBar(true);
 
         setupAdapter();
 
@@ -103,6 +117,85 @@ public class PhotoGalleryFragment extends Fragment {
         super.onDestroy();
         // Clear cache
         clearImageDiskCache();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_photo_gallery, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        mSearchView = (SearchView) searchItem.getActionView();
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "QueryTextSubmit: " + query);
+                QueryPreferences.setStoredQuery(getActivity(), query);
+
+                collapseSearchView(getActivity());
+
+                updateItems();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "QueryTextChange: " + newText);
+                return false;
+            }
+        });
+
+        mSearchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = QueryPreferences.getStoredQuery(getActivity());
+                mSearchView.setQuery(query, false);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_clear:
+                QueryPreferences.setStoredQuery(getActivity(), null);
+
+                if (!mSearchView.isIconified()) {
+                    Log.d(TAG, "SearchView is not iconified, so collapse the SearchView");
+                    collapseSearchView(getActivity());
+                }
+
+                updateItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateItems() {
+        String query = QueryPreferences.getStoredQuery(getActivity());
+        new FetchItemsTask(query, this).execute();
+    }
+
+    private void collapseSearchView(Context context) {
+        // hide the soft keyboard
+        InputMethodManager imm = (InputMethodManager) context
+                .getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+        // collapse the SearchView
+        mSearchView.setIconified(true);
+        mSearchView.setIconified(true);
+    }
+
+    private void showProgressBar(boolean isShow) {
+        if (isShow) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mPhotoRecyclerView.setVisibility(View.INVISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mPhotoRecyclerView.setVisibility(View.VISIBLE);
+        }
     }
 
     private void setupAdapter() {
@@ -178,15 +271,37 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     private class FetchItemsTask extends AsyncTask<Integer, Void, List<GalleryItem>> {
+        private String mQuery;
+        private PhotoGalleryFragment mGalleryFragment;
+
+        public FetchItemsTask(String query, PhotoGalleryFragment fragment) {
+            mQuery = query;
+            mGalleryFragment = fragment;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mGalleryFragment.isResumed()) {
+                mGalleryFragment.showProgressBar(true);
+            }
+        }
+
         @Override
         protected List<GalleryItem> doInBackground(Integer... params) {
-            mBackgroundIsLoading = true;
-            return new FlickrFetchr().fetchItems(params[0]);
+            /*mBackgroundIsLoading = true;
+            return new FlickrFetchr().fetchItems(params[0]);*/
+
+            if (mQuery == null) {
+                return new FlickrFetchr().fetchRecentPhotos();
+            } else {
+                return new FlickrFetchr().searchPhotos(mQuery);
+            }
         }
 
         @Override
         protected void onPostExecute(List<GalleryItem> items) {
-            mBackgroundIsLoading = false;
+            /*mBackgroundIsLoading = false;
 
             if (mLastFetchedPage > 1) {
                 mItems.addAll(items);
@@ -194,7 +309,10 @@ public class PhotoGalleryFragment extends Fragment {
             } else {
                 mItems = items;
                 setupAdapter();
-            }
+            }*/
+            mItems = items;
+            setupAdapter();
+            mGalleryFragment.showProgressBar(false);
         }
     }
 }
